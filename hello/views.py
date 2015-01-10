708d8dbc563af748ceb79b1dbdc628b0c8ba8231
Josh Milburn <joshm@angazadesign.com>
2015-01-09 22:54:13 -0800

    kimono added

--- hello/views.py
+++ hello/views.py
@@ -4,13 +4,16 @@ from django.http import HttpResponse
 from .models import Greeting
 
 import requests
+import os
 
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
+    kimono_key = int(os.environ.get('KIMONO_KEY'))
+    r = requests.get(
+        'http://www.kimonolabs.com/api/25fjm8zy?apikey={}'.format(kimono_key))
     print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse(('<pre>' + r.text + '</pre>'))
 
 
 def db(request):
